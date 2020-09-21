dd9686833c20013d8c885aacf56005fca82d22b1
epson220 <freshsangmin@ajou.ac.kr>
2020-09-22 03:50:08 +0900

    cookie-parser

--- ExpressExample/app8_2.js
+++ ExpressExample/app8_2.js
@@ -14,42 +14,30 @@ app.use(bodyParser.json());
 
 app.use(static(path.join(__dirname, 'public')));
 
-
 var router = express.Router();
 
-var r = 0;
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login 처리함.' + r++);
+router.route('/process/login/:name').post(function(req, res){
+    console.log('/process/login 처리함.');
+
+    var paramName = req.params.name;
 
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
     res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+    res.write('<div><p>Param name : '+ paramName + '</p></div>');
     res.write('<div><p>Param id : ' + paramId + '</p></div>');
     res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-    res.write("<br><br><a href='/public/login2.html'>로그인페이지로 돌아가기</a>");
+    res.write("<br><br><a href='/public/login3.html'>로그인페이지로 돌아가기</a>");
     res.end();
 });
 
+app.use('/', router);
 
-var i = 0;
-
-app.use(function(req, res, next) {
-    
-    console.log('첫 번째 미들웨어에서 요청을 처리함.'+ i++);
-
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-
-    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.write('<h1>Express 서버에서 응답한 결과입니다. </h1>');
-    res.write('<div><p>Param id : ' + paramId + '</p></div>');
-    res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-    res.end();
-
+app.all('*', function(req, res){
+    res.status(404).send('<h1>Error - 페이지를 찾을 수 없습니다.</h1>');
 });
-
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('익스프레스 서버를 시작했습니다. : ' + app.get('port'));
--- ExpressExample/app9.js
+++ ExpressExample/app9.js
@@ -5,21 +5,24 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var static = require('serve-static');
 
+var expressErrorHandler = require('express-error-handler');
+
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})); //post
+app.use(bodyParser.json()); //post 
+app.use(cookieParser());
 
 app.use(static(path.join(__dirname, 'public')));
 
-
 var router = express.Router();
 
-var r = 0;
 router.route('/process/login').post(function(req, res){
-    console.log('/process/login 처리함.' + r++);
+    console.log('/process/login 처리함.');
 
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
@@ -32,24 +35,35 @@ router.route('/process/login').post(function(req, res){
     res.end();
 });
 
+router.route('/process/showCookie').get(function(req, res){
+    console.log('/process/showCookie 호출됨.');
+    //console.log(req.user);
 
-var i = 0;
-
-app.use(function(req, res, next) {
-    
-    console.log('첫 번째 미들웨어에서 요청을 처리함.'+ i++);
-
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
-
-    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.write('<h1>Express 서버에서 응답한 결과입니다. </h1>');
-    res.write('<div><p>Param id : ' + paramId + '</p></div>');
-    res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
-    res.end();
-
+    res.send(req.cookies);
 });
 
+router.route('/process/setUserCookie').get(function(req, res){
+    console.log('/process/setUserCookie 호출됨.');
+
+    res.cookie('user', {
+        id: 'mike',
+        name: '소녀시대',
+        authorized: true
+    });
+
+    res.redirect('/process/showCookie');
+});
+
+app.use('/', router);
+
+var errorHandler = expressErrorHandler({
+    static : {
+        '404' : './public/404.html'
+    }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('익스프레스 서버를 시작했습니다. : ' + app.get('port'));
